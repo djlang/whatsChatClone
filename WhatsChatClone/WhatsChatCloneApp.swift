94a97f3469bd6836ea2108cf065daa9ead50988b
dengjinlang <dengjinlang@aiiiin.com>
2026-05-12 14:05:23 +0800

    Initial Commit

---
+++ WhatsChatClone/WhatsChatCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsChatCloneApp.swift
+//  WhatsChatClone
+//
+//  Created by dj on 2026/5/12.
+//
+
+import SwiftUI
+
+@main
+struct WhatsChatCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
